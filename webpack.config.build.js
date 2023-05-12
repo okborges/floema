9120448d257408298d5df5300644e9517aeaece3
okborges <rafaelborges.av@gmail.com>
2023-05-12 12:06:57 -0300

    setup 2

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -2,15 +2,15 @@ const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const { marge } = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
 
-module.exports = marge(config, {
-  mode: 'production',
+module.exports = merge(config, {
+    mode: 'production',
 
-  output: {
-    path: path.join(__dirname, 'public'),
-  },
+    output: {
+        path: path.join(__dirname, 'public')
+    },
 
-  plugins: [new CleanWebpackPlugin()],
+    plugins: [new CleanWebpackPlugin()]
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,104 @@ const webpack = require('webpack');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
 
 const dirApp = path.join(__dirname, 'app');
-const dirAssets = path.join(__dirname, 'assets');
+const dirShared = path.join(__dirname, 'shared');
 const dirStyles = path.join(__dirname, 'styles');
+const dirNode = 'node_modules';
 
-console.log(dirApp, dirAssets, dirStyles);
+module.exports = {
+    entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
+
+    resolve: {
+        modules: [dirApp, dirShared, dirStyles, dirNode]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEVELOPMENT
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './shared',
+                    to: ''
+                }
+            ]
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+
+        new ImageMinimizerPlugin({
+            minimizer: {
+                implementation: ImageMinimizerPlugin.imageminMinify,
+                options: {
+                    plugins: [
+                        // interlaced: Interlace gif for progressive rendering.
+                        ['gifsicle', { interlaced: true }],
+
+                        // progressive: Lossless conversion to progressive.
+                        ['jpegtran', { progressive: true }],
+
+                        // optimizationLevel (0-7): The optimization level 0 enables a set of
+                        // optimization operations that require minimal effort. There will be
+                        // no changes to image attributes like bit depth or color type, and no
+                        // recompression of existing IDAT datastreams. The optimization level
+                        // 1 enables a single IDAT compression trial. The trial chosen is what
+                        // OptiPNG thinks it’s probably the most effective.
+                        ['optipng', { optimizationLevel: 7 }]
+                    ]
+                }
+            }
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/i,
+                loader: 'file-loader',
+                options: {
+                    name(file) {
+                        return '[hash].[ext]';
+                    }
+                }
+            }
+        ]
+    }
+};
